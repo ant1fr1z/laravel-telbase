d2bc6de7677bc95ec4146c3b6e22b18112be39ae
ant1fr1z <al3ks3y@bigmir.net>
2017-03-28 09:34:33 +0300

    rabota pc

---
+++ resources/views/objects/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('title')
+    БД -> Главная
+@endsection
+
+@section('content')
+    <div class="row">
+        <h3>Все объекты</h3>
+        <div class="col-md-4">
+        </div>
+        <div class="col-md-4">
+test
+        </div><!-- /.col-md-4 -->
+        <div class="col-md-4">
+        </div>
+    </div>
+    </div>
+@endsection
